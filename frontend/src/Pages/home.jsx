14736913351b0e003a461f9e4ef5a0b7e7b0ef59
Ranjith <110477306+Ranjith-41@users.noreply.github.com>
2024-12-12 17:10:07 +0530

    Update home.jsx

--- frontend/src/Pages/home.jsx
+++ frontend/src/Pages/home.jsx
@@ -1,6 +1,4 @@
-import NavbarComponent from "../Components/NavbarComponent";
-import Categories from "../Components/Categories";
-import FooterComponent from "../Components/FooterComponent";
+
 import Hero from "../Components/Hero/Hero";
 
 function Home() {

05ac6c1c838961f8d0e699e64cb2504eff5dd8c1
Ranjith <110477306+Ranjith-41@users.noreply.github.com>
2024-12-12 17:10:36 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import Home from './Pages/home';
+import NavbarComponent from "../Components/NavbarComponent";
+import Categories from "../Components/Categories";
+import FooterComponent from "../Components/FooterComponent";
 function App() {
   return (
     <div className="App">
